18f60eedaaeb2718e11bba9c283ba58ef5e690df
kidsnow <kidsnow0115@gmail.com>
2021-08-05 19:51:18 +0900

    Init!

---
+++ Shared/MetalStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalStudyApp.swift
+//  Shared
+//
+//  Created by Steve Lee on 2021/08/05.
+//
+
+import SwiftUI
+
+@main
+struct MetalStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
